af894c4fedf4c148d6b0ddea2b5f4bea752492ff
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-08-25 14:59:03 -0400

    Turned word_chain into a class

--- word_chain.rb
+++ word_chain.rb
@@ -1,60 +1,69 @@
 require 'debugger'
 require 'set'
 
-def word_chain(start, finish)
-  dictionary = load_dictionary('dictionary.txt')
-  dictionary = filter_dict_by_length(dictionary, start.length)
+class WordChain
+  attr_accessor :dictionary
 
-
-end
-
-def load_dictionary(dict_path)
-  File.readlines(dict_path).map(&:chomp)
-end
-
-def filter_dict_by_length(dictionary, length)
-  dictionary.select{ |word| word.length == length }
-end
-
-def adjacent_words(word, dictionary)
-  regex_array = make_regex_array(word)
-  word_list = []
-  regex_array.each do |regex|
-    word_list << filter_dict_by_regex(dictionary,regex)
+  def initialize(dictionary)
+    @dictionary = load_dictionary(dictionary)
   end
-  word_list.flatten!.uniq!.delete(word)
-  word_list
-end
 
-def make_regex_array(word)
-  [].tap do |regex_arr|
-    word.length.times do |letter_index|
-      temp_word = word.dup
-      temp_word[letter_index] = '.'
-      regex_arr << Regexp.new(temp_word)
+  def word_chain(start, finish)
+    @dictionary = filter_dict_by_length(start.length)
+
+    current_words = [start]
+    adjacent_words = []
+
+    #current_words.each do |word| end
+
+  end
+
+  def load_dictionary(dict_path)
+    File.readlines(dict_path).map(&:chomp)
+  end
+
+  def filter_dict_by_length(length)
+    @dictionary.select{ |word| word.length == length }
+  end
+
+  def filter_dict_by_regex(regex)
+    @dictionary.select{ |word| word.match(regex)}
+  end
+
+  def adjacent_words(word)
+    regex_array = make_regex_array(word)
+    word_list = []
+    regex_array.each do |regex|
+      word_list << filter_dict_by_regex(regex)
+    end
+    word_list.flatten!.uniq!.delete(word)
+    word_list
+  end
+
+  def make_regex_array(word)
+    [].tap do |regex_arr|
+      word.length.times do |letter_index|
+        temp_word = word.dup
+        temp_word[letter_index] = '.'
+        regex_arr << Regexp.new(temp_word)
+      end
     end
   end
+
+  def make_target_words(start, finish)
+    [].tap do |target_words|
+      start.length.times do |index|
+        temp_start = start.dup
+        temp_start[index] = finish[index]
+        target_words << temp_start
+      end
+    end.uniq.reject{|word| word == start}
+  end
 end
 
-def make_target_words(start, finish)
-  [].tap do |target_words|
-    start.length.times do |index|
-      temp_start = start.dup
-      temp_start[index] = finish[index]
-      target_words << temp_start
-    end
-  end.uniq.reject{|word| word == start}
-end
 
 if __FILE__ == $0
-  # p word_chain('cat','got')
-  # p word_chain('cat','ear')
-  # p word_chain('cat','ton')
-  #
-  # p word_chain('duck','rank')
-  # p word_chain('rank','dank')
-  # p word_chain('dank','ruby')
-  p word_chain('duck','lube')
-
+  word_chain = WordChain.new('dictionary.txt')
+  p word_chain.make_regex_array("word")
 end
 

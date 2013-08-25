868ac100873fd33df1776a7ab6560a7278375c79
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-08-25 14:43:08 -0400

    Removed some non-working methods.

--- word_chain.rb
+++ word_chain.rb
@@ -1,59 +1,11 @@
 require 'debugger'
 require 'set'
 
-def word_chain(start,finish, depth = 0, visited_words = Set.new)
-  depth += 1
+def word_chain(start, finish)
   dictionary = load_dictionary('dictionary.txt')
   dictionary = filter_dict_by_length(dictionary, start.length)
-  intermediate = start
-
-  on_the_path_words = []
-  #visited_words = Set.new
-  on_the_path_words << start
-  visited_words << start
-  off_by_one_words = adjacent_words(start,dictionary)
-  off_by_one_words.reject! { |word| visited_words.include?(word) }
 
 
-
-  while intermediate != finish
-    target_words = make_target_words(start, finish)
-
-    target_words.select! { |word| off_by_one_words.include?(word) }
-
-    if target_words.length != 0
-      p "success condition reached. depth: #{depth} word: #{start} visited_word_length: #{visited_words.length}"
-      intermediate = target_words[0]
-      if intermediate == finish
-        p "OMG SUCCESS. depth: #{depth} word: #{start} visited_word_length: #{visited_words.length}"
-        on_the_path_words << intermediate
-      else
-        on_the_path_words << word_chain(target_words[0],finish, depth, visited_words)
-        on_the_path_words.flatten!.uniq!
-        intermediate = on_the_path_words[-1]
-      end
-    else #fail condition
-      p "fail condition reached. depth: #{depth} word: #{start} visited_word_length: #{visited_words.length}"
-      intermediate_finish = nil
-      visited_words = visited_words + off_by_one_words
-      off_by_more_words = words_adjacent_to_visited_words(visited_words, dictionary)
-      # debugger
-      off_by_more_words.each do |word|
-        temp_word_chain = word_chain(word, finish, depth, visited_words)
-        if temp_word_chain[-1] == finish
-          p "FOUND A PATH. depth: #{depth} word: #{start} visited_word_length: #{visited_words.length}"
-          intermediate_finish = word
-        end
-      end
-      if !intermediate_finish.nil?
-        start_to_intermediate_finish = word_chain(start, intermediate_finish, depth, visited_words)
-      end
-      on_the_path_words << start_to_intermediate_finish
-      on_the_path_words << temp_word_chain
-    end
-
-  end
-  on_the_path_words
 end
 
 def load_dictionary(dict_path)
@@ -64,10 +16,6 @@ def filter_dict_by_length(dictionary, length)
   dictionary.select{ |word| word.length == length }
 end
 
-def filter_dict_by_regex(dictionary, regex)
-  dictionary.select{ |word| word.match(regex)}
-end
-
 def adjacent_words(word, dictionary)
   regex_array = make_regex_array(word)
   word_list = []
@@ -76,15 +24,6 @@ def adjacent_words(word, dictionary)
   end
   word_list.flatten!.uniq!.delete(word)
   word_list
-end
-
-def words_adjacent_to_visited_words(visited_words, dictionary)
-  off_by_twos = []
-  visited_words.each do |word|
-    off_by_twos << adjacent_words(word,dictionary)
-  end
-  off_by_twos.flatten!.reject! {|word| visited_words.include?(word)}
-  off_by_twos.uniq
 end
 
 def make_regex_array(word)
@@ -105,9 +44,6 @@ def make_target_words(start, finish)
       target_words << temp_start
     end
   end.uniq.reject{|word| word == start}
-end
-
-def find_chain(start, finish, dictionary)
 end
 
 if __FILE__ == $0
